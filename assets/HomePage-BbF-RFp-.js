4b7fb04e33bdc712f1628b8c77d4a8ba8d97a31c
JorgePap <JorgePap@users.noreply.github.com>
2024-09-17 12:12:22 +0000

    deploy: 7536afe2e0fd85c8647c177153f9ddc6a7b51813

---
+++ assets/HomePage-BbF-RFp-.js
@@ -0,0 +1,20 @@
+import{j as e,T as t,a as n,b as a,R as l,P as x,I as c}from"./index-CX-kk38X.js";const r=({text:s})=>e.jsx("div",{className:"flex ",children:s.split("").map((i,o)=>e.jsx(t,{element:n.H3,variant:a.H2,classes:" transition-transform duration-200 hover:-translate-y-2 lg:hover:-translate-y-6",children:i},o))}),d=({onClick:s})=>e.jsxs("div",{className:"min-h-screen flex flex-col justify-between",children:[e.jsxs("div",{className:"pt-[80px] md:pt-[120px] lg:pt-[120px] flex flex-col text-wrap",children:[e.jsx(r,{text:"Hello"}),e.jsx(r,{text:"I'm   George"}),e.jsx(r,{text:"Papastergiopoulos"})]}),e.jsxs("div",{className:"flex justify-between pb-[30px] md:pb-[50px] lg:pb-[50px]",children:[e.jsx("div",{onClick:s,children:e.jsx(t,{element:n.P,variant:a.BUTTON_SM,text:"Scroll down",classes:"cursor-pointer"})}),e.jsx(t,{element:n.LABEL,variant:a.LABEL,text:"Front-end Developer"})]})]}),p="/george-pap/assets/me-n6QZkejX.jpg",m=()=>e.jsx("div",{className:"min-h-screen pt-[40px] md:pt-[120px] lg:pt-[150px]",children:e.jsxs("div",{className:"flex flex-col lg:flex-row w-full ",children:[e.jsxs("div",{className:"flex flex-col gap-xl basis-1/2",children:[e.jsx(t,{element:n.P,variant:a.H3,text:"Me",color:"text-transparent text-stroke-black dark:text-stroke-white"}),e.jsx(t,{element:n.P,variant:a.P1,text:`Hi, I'm George, and here's a bit about me.
+              I'm 27 years old and a Front-End Developer based in Xanthi,
+              a small city in Greece, with a strong passion for web development
+              and a love for creating interactive, responsive web experiences.
+              I'm always excited to explore new technologies and creative solutions.`}),e.jsx(t,{element:n.P,variant:a.P1,text:`As a self-taught developer, I constantly strive to stay
+              updated on the latest trends and best practices in front-end development.
+              I believe that continuous learning is key to growth, so I make it a point
+              to expand my knowledge and refine my skills regularly. My goal is to deliver
+              high-quality work that balances both creativityand functionality,
+              ensuring that users have a seamless and engaging experience.`}),e.jsx(t,{element:n.P,variant:a.P1,text:`I have a particular interest in React and enjoy leveraging its capabilities
+              to build dynamic, user-friendly interfaces. My focus also includes responsive
+              design and performance optimization, as I believe these elements are crucial
+              for a great user experience. I'm eager to dive into ambitious projects where
+              I can contribute, grow, and make a meaningful impact.`}),e.jsx(t,{element:n.P,variant:a.P1,text:`Currently, I'm looking for my first professional opportunity to collaborate with
+              a dynamic team and work on challenging projects that push the boundaries of what web
+              development can achieve. I'm confident that my passion, dedication, and drive to learn
+              will make me a valuable addition to any project.`})]}),e.jsx("div",{className:"flex basis-1/2 justify-center items-center",children:e.jsx("img",{src:p,className:`border-[1px] border-neutral-2 dark:border-neutral-1 rounded-[70%_30%_30%_70%_/_60%_40%_60%_40%] w-[250px] h-[250px] mt-[40px] mb-[40px]
+           md:w-[350px] md:h-[350px]
+           lg:w-[400px] lg:h-[400px] lg:mt-[100px]
+           xl:w-[430px] xl:h-[430px] `})})]})}),v=()=>{const s=()=>{window.scrollBy({top:window.innerHeight,behavior:"smooth"})};return e.jsxs("div",{className:"px-[30px] md:px-[115px] lg:px-[125px] overflow-x-hidden ",children:[e.jsx(d,{onClick:s}),e.jsx(m,{}),e.jsx(l,{to:x.PORTFOLIO,text:"Portfolio",left:!1,routeIcon:c.ARROW_RIGHT})]})};export{v as HomePage,v as default};
